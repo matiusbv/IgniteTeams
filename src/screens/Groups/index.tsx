ab692e53d4cd1e9022dcc7f6a27b0e24f075f14d
Marcio Vasconcelos <matiusbv@gmail.com>
2022-09-20 20:05:01 -0300

    Criando Componente Header

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,12 @@
 
 
+import { Header } from '@components/Header';
 import { Container} from './styles';
 
 export function Groups() {
   return (
     <Container>
-     
+     <Header />
     </Container>
   );
 }
