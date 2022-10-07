a80f6703b5b12fc927d55e34c29c89856a9f6350
Marcio Vasconcelos <matiusbv@gmail.com>
2022-10-07 20:29:02 -0300

    Identificando o foco na tela

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { FlatList } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
@@ -8,6 +8,7 @@ import { Highlight } from '@components/Highlight';
 import { Container} from './styles';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
+import { groupsGetAll } from '@storage/group/groupsGetAll';
 
 export function Groups() {
 const [groups, setGroups] = useState<string[]>([]);
@@ -16,7 +17,21 @@ const navigation = useNavigation();
 
 function handleNewGroup(){
   navigation.navigate('new');
+  
 }
+
+async function fetchGroups(){
+  try{
+    const data = await groupsGetAll()   
+    setGroups(data);
+  }catch(error){
+    console.log(error);
+  }
+}
+
+useFocusEffect(useCallback(() => {
+  fetchGroups();
+}, []));
 
   return (
     <Container>
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,6 +3,7 @@ import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
 import { useNavigation } from '@react-navigation/native'
+import { groupCreate } from '@storage/group/groupCreate'
 import { useState } from 'react'
 import { Container, Content, Icon } from './styles'
 
@@ -13,9 +14,15 @@ export function NewGroup(){
 
     const navigation = useNavigation();
 
-    function handleNew(){
+    async function handleNew(){
+        try{
+        await groupCreate(group);
         navigation.navigate('players', {group});
+    } catch (error) {
+        console.log(error)
     }
+
+}
 
     return (
         <Container>
